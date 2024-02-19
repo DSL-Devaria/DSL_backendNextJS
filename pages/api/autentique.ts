7907575967f0c4f0b38bdf4fb017b6233a669396
StacyFranco <stacyfranco@hotmail.com>
2024-02-19 16:24:43 -0300

    ably adicionado para fazer websocket

--- pages/api/autentique.ts
+++ pages/api/autentique.ts
@@ -1,6 +1,7 @@
 import { politicaCORS } from "@/middlewares/politicaCORS";
 import { RespostaPadraoMsg } from "@/types/respostaPadraoMsg";
 import { NextApiRequest, NextApiResponse } from "next";
+import Ably from 'ably'
 
 const endpointAutentique = async (
     req: NextApiRequest,
@@ -9,9 +10,31 @@ const endpointAutentique = async (
 
 
     if (req.method === 'POST') {
-        const WebhookData= JSON.stringify(req.body)
+        const WebhookData = JSON.parse(JSON.stringify(req.body))
         console.log('webhook', WebhookData)
-        return res.status(200).json(WebhookData)
-    }}
+        console.log('webhook teste docID', WebhookData.documento.uuid)
 
-    export default politicaCORS(endpointAutentique)
+        
+
+        // --------ABLY---------------
+       /* const { ABLY_API_KEY } = process.env;
+        const ably = new Ably.Realtime.Promise(`${ABLY_API_KEY}`);
+
+        await ably.connection.once("connected");
+        console.log("Connected to Ably!");
+
+        // get the channel to subscribe to
+        const channel = ably.channels.get("autentique");
+
+        await channel.subscribe("webhook", (message) => {
+            console.log("you recive this update webhook ==> " + message.data);
+        });
+
+        // Publish a message or two
+        await channel.publish("webhook", WebhookData);
+*/
+        return res.status(200).json(WebhookData)
+    }
+}
+
+export default politicaCORS(endpointAutentique)
