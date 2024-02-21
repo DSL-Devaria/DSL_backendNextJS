e5eac8f5864a4195cd23fa59bc5ba02bf267abf0
StacyFranco <stacyfranco@hotmail.com>
2024-02-21 11:05:42 -0300

    mudança local de adicionar partes no webhooksimplificado

--- pages/api/autentique.ts
+++ pages/api/autentique.ts
@@ -8,67 +8,71 @@ const endpointAutentique = async (
     req: NextApiRequest,
     res: NextApiResponse<RespostaPadraoMsg | any>) => {
 
-        function transformToJSON(data: any): string {
-            let result: any = {};
-            for (let key in data) {
-                let keys = key.split('[').map(k => k.replace(']', ''));
-                let current = result;
-                for (let i = 0; i < keys.length; i++) {
-                    let keyPart = keys[i];
-                    if (!(keyPart in current)) {
-                        if (i < keys.length - 1 && !Number.isNaN(Number(keys[i + 1]))) {
-                            current[keyPart] = [];
-                        } else {
-                            current[keyPart] = {};
-                        }
-                    }
-                    if (i === keys.length - 1) {
-                        current[keyPart] = data[key];
+    function transformToJSON(data: any): string {
+        let result: any = {};
+        for (let key in data) {
+            let keys = key.split('[').map(k => k.replace(']', ''));
+            let current = result;
+            for (let i = 0; i < keys.length; i++) {
+                let keyPart = keys[i];
+                if (!(keyPart in current)) {
+                    if (i < keys.length - 1 && !Number.isNaN(Number(keys[i + 1]))) {
+                        current[keyPart] = [];
                     } else {
-                        current = current[keyPart];
+                        current[keyPart] = {};
                     }
                 }
+                if (i === keys.length - 1) {
+                    current[keyPart] = data[key];
+                } else {
+                    current = current[keyPart];
+                }
             }
-            return JSON.stringify(result, null, 4);
         }
+        return JSON.stringify(result, null, 4);
+    }
 
 
     if (req.method === 'POST') {
         const WebhookData = JSON.parse(transformToJSON(req.body))
         const WebhookDataSimplificada = {
-            partes:[],
-            documento:{
+            partes: (
+                WebhookData.partes.map((parte: any) => {
+
+                    return ({
+                        id: parte.uuid,
+                        nome: parte.nome,
+                        email: parte.email,
+                        ordem: parte.ordem,
+                        funcao: parte.funcao,
+                        mail: parte.mail,
+                        visualizado: parte.visualizado?.created,
+                        assinado: parte.assinado?.created,
+
+                    })
+                })
+            ),
+            documento: {
                 id: WebhookData.documento.uuid,
-                nome:WebhookData.documento.nome,
-                publicado:WebhookData.documento.publicado,
-                updated:WebhookData.documento.updated
+                nome: WebhookData.documento.nome,
+                publicado: WebhookData.documento.publicado,
+                updated: WebhookData.documento.updated
 
             },
-            remetente:{
+            remetente: {
                 nome: WebhookData.remetente.nome,
                 email: WebhookData.remetente.email
             },
-            arquivo:{
-                original:WebhookData.arquivo.original,
-                assinado:WebhookData.arquivo.assinado
+            arquivo: {
+                original: WebhookData.arquivo.original,
+                assinado: WebhookData.arquivo.assinado
             },
         };
-        WebhookData.partes.forEach((parte:any) => {
-            WebhookDataSimplificada.partes.push({
-                id:parte.uuid,
-                nome:parte.nome,
-                email:parte.email,
-                ordem:parte.ordem,
-                funcao: parte.funcao,
-                mail:parte.mail,
-                visualizado:parte.visualizado?.created,
-                assinado:parte.assinado?.created,
-            })
-        });
-       
+
+
         //console.log('webhook', WebhookDataSimplificada)
 
-        
+
 
         // --------ABLY---------------
         const { ABLY_API_KEY } = process.env;
@@ -86,9 +90,9 @@ const endpointAutentique = async (
 
         // Publish a message or two
         await channel.publish("webhook", WebhookData);
-        
+      
 
-        return res.status(200).json(WebhookData)
+        return res.status(200).json(WebhookDataSimplificada)
     }
 }
 
